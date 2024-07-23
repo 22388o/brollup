b1123e649961b09c64e005da904a3974782027ea
brqgoo <100725376+brqgoo@users.noreply.github.com>
2024-07-23 12:04:11 +0300

    Payload struct 📦

--- src/txo/payload.rs
+++ src/txo/payload.rs
@@ -1,9 +1,8 @@
-#![allow(dead_code)]
+use bit_vec::BitVec;
 
 use crate::{
     serialize::{to_csv_script_encode, CSVFlag},
     taproot::{TapLeaf, TapRoot},
-    well_known::operator,
 };
 use musig2::secp256k1::{self, XOnlyPublicKey};
 
@@ -11,13 +10,74 @@ type Bytes = Vec<u8>;
 type Key = XOnlyPublicKey;
 
 pub struct Payload {
-    fresh_operator_key_dynamic: Key,
-    vtxo_projector_msg_senders_agg_sig: Bytes,
-    vtxo_projector_operator_s_commitment: Bytes,
-    connector_projector_msg_senders_agg_sig: Bytes,
-    connector_projector_operator_s_commitment: Bytes,
+    // msg.sender keys
     msg_senders: Vec<Key>,
-    s_commitments: Vec<Bytes>,
-    entries: Vec<Bytes>,
-    entries_zero_bits_padding: u8,
+    // s commitments
+    s_commitments: Vec<[u8; 32]>,
+    // Fresh operator key 🔑
+    fresh_operator_key_dynamic: Key,
+    // VTXO projector signatures 🎥
+    vtxo_projector_msg_senders_agg_sig: [u8; 64],
+    vtxo_projector_operator_s_commitment: [u8; 32],
+    // Connector projector signatures 🎥
+    connector_projector_msg_senders_agg_sig: [u8; 64],
+    connector_projector_operator_s_commitment: [u8; 32],
+    // Entries
+    entries: Vec<BitVec>,
+}
+
+impl Payload {
+    pub fn new(
+        msg_senders: Vec<Key>,
+        s_commitments: Vec<[u8; 32]>,
+        fresh_operator_key_dynamic: Key,
+        vtxo_projector_msg_senders_agg_sig: [u8; 64],
+        vtxo_projector_operator_s_commitment: [u8; 32],
+        connector_projector_msg_senders_agg_sig: [u8; 64],
+        connector_projector_operator_s_commitment: [u8; 32],
+        entries: Vec<BitVec>,
+    ) -> Payload {
+        Payload {
+            msg_senders,
+            s_commitments,
+            fresh_operator_key_dynamic,
+            vtxo_projector_msg_senders_agg_sig,
+            vtxo_projector_operator_s_commitment,
+            connector_projector_msg_senders_agg_sig,
+            connector_projector_operator_s_commitment,
+            entries,
+        }
+    }
+
+    pub fn data_to_be_pushed(&self) -> Bytes {
+        let mut data = Vec::<u8>::new();
+
+        // Start with adding the fresh operator key 🔑
+        data.extend(self.fresh_operator_key_dynamic.serialize().to_vec());
+
+        // Add vtxo_projector_msg_senders_agg_sig (64 bytes)
+        data.extend(self.vtxo_projector_msg_senders_agg_sig);
+
+        // Add vtxo_projector_operator_s_commitment (32 bytes)
+        data.extend(self.vtxo_projector_operator_s_commitment);
+
+        // Add connector_projector_msg_senders_agg_sig (64 bytes)
+        data.extend(self.connector_projector_msg_senders_agg_sig);
+
+        // Add connector_projector_operator_s_commitment (32 bytes)
+        data.extend(self.connector_projector_operator_s_commitment);
+
+        let mut entries = BitVec::new();
+
+        for entry in self.entries.iter() {
+            entries.extend(entry);
+        }
+
+        let zero_bits_padded: u8 = 8 - (entries.len() % 8) as u8;
+        data.push(zero_bits_padded);
+
+        data.extend(entries.to_bytes());
+
+        data
+    }
 }
