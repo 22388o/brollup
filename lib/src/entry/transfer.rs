eade8692ef4c02e55b20f85e27c9ebed6cb469f1
brqgoo <100725376+brqgoo@users.noreply.github.com>
2024-07-26 14:29:14 +0300

    transfer

--- lib/src/entry/transfer.rs
+++ lib/src/entry/transfer.rs
@@ -1,11 +1,8 @@
 #![allow(dead_code)]
 
+use crate::value::{account::Account, amount::ShortAmount, ToBitVec};
+
 use bit_vec::BitVec;
-use musig2::secp256k1::XOnlyPublicKey;
-
-use crate::value::{amount::ShortAmount, ToBitVec};
-
-type Account = XOnlyPublicKey;
 
 pub enum Fallback {
     Bare,
@@ -16,29 +13,22 @@ pub struct Transfer {
     from: Account,
     to: Account,
     amount: ShortAmount,
-    fallback: Fallback,
+    fallback: Option<Fallback>,
 }
 
 impl Transfer {
-    pub fn new_with_bare_fallback(from: Account, to: Account, amount: u32) -> Transfer {
+    pub fn new(from: Account, to: Account, amount: u32) -> Transfer {
         Transfer {
             from,
             to,
             amount: ShortAmount(amount),
-            fallback: Fallback::Bare,
+            fallback: None,
         }
     }
+}
 
-    pub fn new_with_virtual_fallback(from: Account, to: Account, amount: u32) -> Transfer {
-        Transfer {
-            from,
-            to,
-            amount: ShortAmount(amount),
-            fallback: Fallback::Virtual,
-        }
-    }
-
-    pub fn to_non_compact_bit_vec(&self) -> BitVec {
+impl ToBitVec for Transfer {
+    fn to_bit_vec(&self) -> BitVec {
         let mut bit_vec = BitVec::new();
 
         // Transfer or call
@@ -47,23 +37,11 @@ impl Transfer {
         // Transfer
         bit_vec.push(false);
 
-        // Fallback type
-        match self.fallback {
-            Fallback::Bare => bit_vec.push(false),
-            Fallback::Virtual => bit_vec.push(true),
-        }
+        // From
+        bit_vec.extend(self.from.to_bit_vec());
 
-        // Non-compact from
-        bit_vec.push(false);
-        let from_key_bytes = self.from.serialize().to_vec();
-        let from_key_bits = BitVec::from_bytes(&from_key_bytes);
-        bit_vec.extend(from_key_bits);
-
-        // Non-compact to
-        bit_vec.push(false);
-        let to_key_bytes = self.to.serialize().to_vec();
-        let to_key_bits = BitVec::from_bytes(&to_key_bytes);
-        bit_vec.extend(to_key_bits);
+        // To
+        bit_vec.extend(self.to.to_bit_vec());
 
         // Value
         bit_vec.extend(self.amount.to_bit_vec());
