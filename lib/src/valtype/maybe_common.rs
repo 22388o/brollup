ffbc4aef0ca897fd4072f6b1dc14ccbfe2717303
brqgoo <100725376+brqgoo@users.noreply.github.com>
2024-07-31 19:38:21 +0300

    common_index_from_u8

--- lib/src/valtype/maybe_common.rs
+++ lib/src/valtype/maybe_common.rs
@@ -35,59 +35,67 @@ impl<T: MaybeCommonType + CompactPayloadEncoding> CompactPayloadEncoding for May
                 // Common bit: true
                 bit_vec.push(true);
                 // 3-bit common index encoding
-                match common_index {
-                    0 => {
-                        // 0b000
-                        bit_vec.push(false);
-                        bit_vec.push(false);
-                        bit_vec.push(false);
-                    }
-                    1 => {
-                        // 0b001
-                        bit_vec.push(false);
-                        bit_vec.push(false);
-                        bit_vec.push(true);
-                    }
-                    2 => {
-                        // 0b010
-                        bit_vec.push(false);
-                        bit_vec.push(true);
-                        bit_vec.push(false);
-                    }
-                    3 => {
-                        // 0b011
-                        bit_vec.push(false);
-                        bit_vec.push(true);
-                        bit_vec.push(true);
-                    }
-                    4 => {
-                        // 0b100
-                        bit_vec.push(true);
-                        bit_vec.push(false);
-                        bit_vec.push(false);
-                    }
-                    5 => {
-                        // 0b101
-                        bit_vec.push(true);
-                        bit_vec.push(false);
-                        bit_vec.push(true);
-                    }
-                    6 => {
-                        // 0b110
-                        bit_vec.push(true);
-                        bit_vec.push(true);
-                        bit_vec.push(false);
-                    }
-                    7 => {
-                        // 0b111
-                        bit_vec.push(true);
-                        bit_vec.push(true);
-                        bit_vec.push(true);
-                    }
-                    _ => panic!("Common index is 3-bit-long."),
-                }
+                bit_vec.extend(common_index_from_u8(common_index));
                 bit_vec
             }
         }
     }
+}
+
+fn common_index_from_u8(common_index: &u8) -> BitVec {
+    let mut bit_vec = BitVec::new();
+
+    // 3-bit common index encoding
+    match common_index {
+        0 => {
+            // 0b000
+            bit_vec.push(false);
+            bit_vec.push(false);
+            bit_vec.push(false);
+        }
+        1 => {
+            // 0b001
+            bit_vec.push(false);
+            bit_vec.push(false);
+            bit_vec.push(true);
+        }
+        2 => {
+            // 0b010
+            bit_vec.push(false);
+            bit_vec.push(true);
+            bit_vec.push(false);
+        }
+        3 => {
+            // 0b011
+            bit_vec.push(false);
+            bit_vec.push(true);
+            bit_vec.push(true);
+        }
+        4 => {
+            // 0b100
+            bit_vec.push(true);
+            bit_vec.push(false);
+            bit_vec.push(false);
+        }
+        5 => {
+            // 0b101
+            bit_vec.push(true);
+            bit_vec.push(false);
+            bit_vec.push(true);
+        }
+        6 => {
+            // 0b110
+            bit_vec.push(true);
+            bit_vec.push(true);
+            bit_vec.push(false);
+        }
+        7 => {
+            // 0b111
+            bit_vec.push(true);
+            bit_vec.push(true);
+            bit_vec.push(true);
+        }
+        _ => panic!("Common index is 3-bit-long."),
+    }
+    bit_vec
 }
