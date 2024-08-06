b23421f7d9f3b2b2ea65507b124dbc28a01ac182
brqgoo <100725376+brqgoo@users.noreply.github.com>
2024-08-07 02:46:57 +0300

    Update push.rs

--- lib/src/serialize/push.rs
+++ lib/src/serialize/push.rs
@@ -102,10 +102,10 @@ fn encode_multi_push(data: &Bytes, flag: PushFlag) -> Bytes {
 
     for chunk in chunks {
         match flag {
-            // Use OP_PUSHDATA encoding for in-script witness pushes.
+            // Use OP_PUSHDATA encoding for in-script pushes.
             PushFlag::ScriptPush => encoded.extend(chunk.prefix_pushdata()),
 
-            // Use varint encoding for out-script witness pushes.
+            // Use varint encoding for witness pushes.
             _ => encoded.extend(chunk.prefix_compact_size()),
         }
     }
