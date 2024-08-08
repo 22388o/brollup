931036112c9f9f60e355b528d96e4f797497278a
brqgoo <100725376+brqgoo@users.noreply.github.com>
2024-08-08 04:09:08 +0300

    Update schnorr.rs

--- lib/src/signature/schnorr.rs
+++ lib/src/signature/schnorr.rs
@@ -14,7 +14,6 @@ pub enum SignError {
     SignatureParseError,
     InvalidScalar,
     InvalidPoint,
-    InvalidSecretKey,
 }
 pub trait Sign {
     fn sign(&self, secret_key: [u8; 32], prev_state_hash: [u8; 32]) -> Result<[u8; 64], SignError>;
